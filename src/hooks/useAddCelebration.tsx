2b326ae65d8760139170523140fe13f9b7939fc7
yuki-kai <issi0430bjc@gmail.com>
2024-11-12 00:10:23 +0900

    お祝い保存後の遷移先を調整

--- src/hooks/useAddCelebration.tsx
+++ src/hooks/useAddCelebration.tsx
@@ -1,14 +1,17 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { Celebration } from "../models/Celebration";
+import { useNavigation } from "@react-navigation/native";
 
 const useAddCelebration = () => {
+  const navigation = useNavigation();
   const addCelebration = (celebration: Celebration) => {
     console.log("useAddCelebration: " + celebration);
     const collectionPath = collection(db, celebration.path);
     addDoc(collectionPath, { ...celebration.toDto() })
       .then((docRef) => {
         console.log("成功" + docRef);
+        navigation.goBack();
       })
       .catch((error) => {
         console.log("失敗" + error);

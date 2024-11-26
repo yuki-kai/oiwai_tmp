a00f7a1d306597a24e18a5a4c0d8c8c69dfab92a
yuki-kai <issi0430bjc@gmail.com>
2024-11-27 08:14:18 +0900

    インデントのサイズは2

--- src/repositories/celebration.repository.ts
+++ src/repositories/celebration.repository.ts
@@ -1,58 +1,69 @@
 import { CelebrationDto } from "../types/celebration";
-import { addDoc, collection, CollectionReference, DocumentData, FirestoreDataConverter, getDocs, orderBy, query, QueryDocumentSnapshot, serverTimestamp, SnapshotOptions } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  DocumentData,
+  FirestoreDataConverter,
+  getDocs,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  serverTimestamp,
+  SnapshotOptions,
+} from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 
 const celebrationConverter: FirestoreDataConverter<CelebrationDto> = {
-    fromFirestore(
-		snapshot: QueryDocumentSnapshot,
-		options: SnapshotOptions,
-    ): CelebrationDto {
-		const data = snapshot.data(options);
-		return {
-			docId: snapshot.id,
-			dayName: data.dayName,
-			date: data.date,
-			createdAt: data.createdAt,
-			updatedAt: data.updatedAt,
-		};
-    },
-    toFirestore(celebration: CelebrationDto): DocumentData {
-		return {
-			dayName: celebration.dayName,
-			date: celebration.date,
-			createdAt: celebration.createdAt ? celebration.createdAt : serverTimestamp(),
-			updatedAt: serverTimestamp(),
-		};
-    },
-  };
+  fromFirestore(snapshot: QueryDocumentSnapshot, options: SnapshotOptions): CelebrationDto {
+    const data = snapshot.data(options);
+    return {
+      docId: snapshot.id,
+      dayName: data.dayName,
+      date: data.date,
+      createdAt: data.createdAt,
+      updatedAt: data.updatedAt,
+    };
+  },
+  toFirestore(celebration: CelebrationDto): DocumentData {
+    return {
+      dayName: celebration.dayName,
+      date: celebration.date,
+      createdAt: celebration.createdAt ? celebration.createdAt : serverTimestamp(),
+      updatedAt: serverTimestamp(),
+    };
+  },
+};
 
 export class CelebrationRepository {
-	private path: string;
-	private collectionRef: CollectionReference;
+  private path: string;
+  private collectionRef: CollectionReference;
 
-	constructor(userId: string) {
-		console.log("CelebrationRepository: " + userId);
-		this.path = `users/${userId}/celebrations`;
-		this.collectionRef = collection(db, this.path).withConverter(celebrationConverter);
-	}
+  constructor(userId: string) {
+    console.log("CelebrationRepository: " + userId);
+    this.path = `users/${userId}/celebrations`;
+    this.collectionRef = collection(db, this.path).withConverter(
+      celebrationConverter
+    );
+  }
 
-	public async getCelebrationList(): Promise<CelebrationDto[]> {
-		const snapshot = await getDocs(query(this.collectionRef, orderBy("date")));
-		return snapshot.docs.map((doc) => {
-			const celebration = doc.data();
-			return {
-				docId: doc.id,
-				dayName: celebration.dayName,
-				date: celebration.date,
-			};
-		});
-	}
+  public async getCelebrationList(): Promise<CelebrationDto[]> {
+    const snapshot = await getDocs(query(this.collectionRef, orderBy("date")));
+    return snapshot.docs.map((doc) => {
+      const celebration = doc.data();
+      return {
+        docId: doc.id,
+        dayName: celebration.dayName,
+        date: celebration.date,
+      };
+    });
+  }
 
-	public async createCelebration(celebration: CelebrationDto): Promise<void> {
-		await addDoc(this.collectionRef, {
-			dayName: celebration.dayName,
-			date: celebration.date,
-		});
-		// TODO: エラーハンドリング
-	}
+  public async createCelebration(celebration: CelebrationDto): Promise<void> {
+    await addDoc(this.collectionRef, {
+      dayName: celebration.dayName,
+      date: celebration.date,
+    });
+    // TODO: エラーハンドリング
+  }
 }

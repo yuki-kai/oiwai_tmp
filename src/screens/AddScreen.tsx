48ce783c265f9ea22f004a738d94b770a033885a
yuki-kai <issi0430bjc@gmail.com>
2024-12-10 07:11:39 +0900

    お祝いの作成・更新にreact-hook-formを使用(コンポーネント化は後回し)

--- src/screens/AddScreen.tsx
+++ src/screens/AddScreen.tsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   View,
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
   KeyboardAvoidingView,
+  Text,
+  TextInput,
 } from "react-native";
-import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import AddButton from "../components/AddButton";
 import useAddaddCelebration from "../hooks/useAddCelebration";
 import { Celebration } from "../models/Celebration";
-import TextForm from "../components/TextForm";
 import { convertDateString } from "../utils/dateFormat";
-import DateForm from "../components/DateForm";
+import { useForm, Controller } from "react-hook-form";
+import { InputCelebration } from "../types/celebration";
+// import TextForm from "../components/TextForm";
+// import DateForm from "../components/DateForm";
 
 export default function AddScreen() {
   const { addCelebration } = useAddaddCelebration();
-  const [dayName, setDayName] = useState("");
-  const [date, setDate] = useState(new Date());
+  const { control, handleSubmit, formState } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      dayName: "",
+      date: new Date(),
+    },
+  });
 
-  // 年月日変更時に呼ばれる
-  const onDateChange = (event: DateTimePickerEvent, selectedDate: Date | undefined) => {
-    setDate(selectedDate || date);
-  };
-
-  const handleAddCelebration = () => {
-    const dateString = convertDateString(date);
-    const celebration = Celebration.create({ dayName, date: dateString });
+  const handleAddCelebration = (data: InputCelebration) => {
+    const dateString = convertDateString(data.date);
+    const celebration = Celebration.create({ dayName: data.dayName, date: dateString });
     addCelebration(celebration);
   };
 
@@ -34,23 +38,57 @@ export default function AddScreen() {
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View>
-          <TextForm
-            label="お祝いする日"
-            value={dayName}
-            onChangeText={(value: string) => setDayName(value)}
-            placeholder="お祝いしたい日を入力してください"
-          />
-          <DateForm
-            label="年月日"
-            value={date}
-            onChangeDate={onDateChange}
-          />
+          <View style={styles.formWrapper}>
+            <Text style={styles.label}>お祝いする日</Text>
+            <Controller
+              control={control}
+              name="dayName"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  value={value}
+                  placeholder="お祝いしたい日を入力してください"
+                  onChangeText={onChange}
+                />
+              )}
+              rules={{
+                required: "入力が必要です。",
+                maxLength: { value: 20, message: "20文字以内で入力してください" },
+              }}
+            />
+            {formState.errors.dayName && typeof formState.errors.dayName.message === "string" && (
+              <Text style={styles.errorMessage}>{formState.errors.dayName.message}</Text>
+            )}
+          </View>
+
+          <View style={styles.formWrapper}>
+            <Text style={styles.label}>年月日</Text>
+            <Controller
+              control={control}
+              name="date"
+              render={({ field: { onChange, value } }) => (
+                <DateTimePicker
+                  style={styles.input}
+                  value={value}
+                  display="spinner"
+                  locale="ja-JP"
+                  onChange={(event, value) => {
+                    onChange(value);
+                  }}
+                />
+              )}
+              rules={{ required: "入力が必要です。" }}
+            />
+            {formState.errors.date && typeof formState.errors.date.message === "string" && (
+              <Text style={styles.errorMessage}>{formState.errors.date.message}</Text>
+            )}
+          </View>
         </View>
       </TouchableWithoutFeedback>
 
       <AddButton
         label="追加する"
-        handleButtonPress={handleAddCelebration}
+        handleButtonPress={handleSubmit(handleAddCelebration)}
       />
     </KeyboardAvoidingView>
   );
@@ -60,5 +98,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  formWrapper: {
+    backgroundColor: "#fff",
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+  },
+  label: {
+    fontSize: 12,
+    marginBottom: 2,
+  },
+  input: {
+    borderColor: "#969696",
+    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+  },
+  errorMessage: {
+    color: "red",
   },
 });
--- src/screens/EditScreen.tsx
+++ src/screens/EditScreen.tsx
@@ -1,37 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   View,
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
   KeyboardAvoidingView,
+  Text,
+  TextInput,
 } from "react-native";
-import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
 import AddButton from "../components/AddButton";
 import useEditCelebration from "../hooks/useEditCelebration";
 import { Celebration } from "../models/Celebration";
-import TextForm from "../components/TextForm";
 import { convertDateString, dateFromString } from "../utils/dateFormat";
-import DateForm from "../components/DateForm";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { RootStackParamList } from "../types/route";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useForm, Controller } from "react-hook-form";
+import { InputCelebration } from "../types/celebration";
+// import TextForm from "../components/TextForm";
+// import DateForm from "../components/DateForm";
 
 export default function EditScreen() {
   const { editCelebration } = useEditCelebration();
   const route = useRoute<RouteProp<RootStackParamList, "Detail">>();
-  const [dayName, setDayName] = useState(route.params.celebration.dayName);
-  const [date, setDate] = useState(new Date(dateFromString(route.params.celebration.date)));
+  const { control, handleSubmit, formState } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      dayName: route.params.celebration.dayName,
+      date: new Date(dateFromString(route.params.celebration.date)),
+    },
+  });
 
-  // 年月日変更時に呼ばれる
-  const onDateChange = (event: DateTimePickerEvent, selectedDate: Date | undefined) => {
-    setDate(selectedDate || date);
-  };
-
-  const handleEditCelebration = () => {
-    const dateString = convertDateString(date);
+  const handleEditCelebration = (data: InputCelebration) => {
+    const dateString = convertDateString(data.date);
     const celebration = Celebration.create({
       docId: route.params.celebration.docId,
-      dayName: dayName,
+      dayName: data.dayName,
       date: dateString,
     });
     editCelebration(celebration);
@@ -41,23 +45,57 @@ export default function EditScreen() {
     <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View>
-          <TextForm
-            label="お祝いする日"
-            value={dayName}
-            onChangeText={(value: string) => setDayName(value)}
-            placeholder="お祝いしたい日を入力してください"
-          />
-          <DateForm
-            label="年月日"
-            value={date}
-            onChangeDate={onDateChange}
-          />
+          <View style={styles.formWrapper}>
+            <Text style={styles.label}>お祝いする日</Text>
+            <Controller
+              control={control}
+              name="dayName"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  value={value}
+                  placeholder="お祝いしたい日を入力してください"
+                  onChangeText={onChange}
+                />
+              )}
+              rules={{
+                required: "入力が必要です。",
+                maxLength: { value: 20, message: "20文字以内で入力してください" },
+              }}
+            />
+            {formState.errors.dayName && typeof formState.errors.dayName.message === "string" && (
+              <Text style={styles.errorMessage}>{formState.errors.dayName.message}</Text>
+            )}
+          </View>
+
+          <View style={styles.formWrapper}>
+            <Text style={styles.label}>年月日</Text>
+            <Controller
+              control={control}
+              name="date"
+              render={({ field: { onChange, value } }) => (
+                <DateTimePicker
+                  style={styles.input}
+                  value={value}
+                  display="spinner"
+                  locale="ja-JP"
+                  onChange={(event, value) => {
+                    onChange(value);
+                  }}
+                />
+              )}
+              rules={{ required: "入力が必要です。" }}
+            />
+            {formState.errors.date && typeof formState.errors.date.message === "string" && (
+              <Text style={styles.errorMessage}>{formState.errors.date.message}</Text>
+            )}
+          </View>
         </View>
       </TouchableWithoutFeedback>
 
       <AddButton
         label="更新する"
-        handleButtonPress={handleEditCelebration}
+        handleButtonPress={handleSubmit(handleEditCelebration)}
       />
     </KeyboardAvoidingView>
   );
@@ -67,5 +105,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  formWrapper: {
+    backgroundColor: "#fff",
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+  },
+  label: {
+    fontSize: 12,
+    marginBottom: 2,
+  },
+  input: {
+    borderColor: "#969696",
+    borderWidth: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    borderRadius: 4,
+  },
+  errorMessage: {
+    color: "red",
   },
 });
